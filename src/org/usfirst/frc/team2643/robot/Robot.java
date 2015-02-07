5d584653f6859bbd4d1949e6a4301880a0e4e305
Team2643 <team2643@gmail.com>
2015-02-06 21:52:22 -0800

    teleop is done, and most of the others too

--- src/org/usfirst/frc/team2643/robot/Robot.java
+++ src/org/usfirst/frc/team2643/robot/Robot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2643.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -12,35 +13,56 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 import org.usfirst.frc.team2643.robot.commands.ExampleCommand;
 import org.usfirst.frc.team2643.robot.subsystems.ExampleSubsystem;
-// DIVIDE ENCODERS BY 360 && MULTIPLY BY DIAMETER AND Math.PI//
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+
 public class Robot extends IterativeRobot 
 {
+	//Drive motors
 	Talon forwardLeftMotor = new Talon(5);
 	Talon backLeftMotor = new Talon(6);
 	Talon forwardRightMotor = new Talon(7);
 	Talon backRightMotor = new Talon(9);
-	SpeedController linearSlideMotor1;
-	SpeedController linearSlideMotor2;
+	
 	SpeedController strafeMotor1;
 	SpeedController strafeMotor2;
+	
+	//Linear slide
+	SpeedController linearSlideMotor1;
+	SpeedController linearSlideMotor2;
+	
+	//Digital Inputs
 	DigitalInput slideSwitchTop;
 	DigitalInput slideSwitchBottom;
-	DigitalInput frontLeftLimitSwitch;
-	DigitalInput frontRightLimitSwitch;
-	Joystick padDriver = new Joystick(1);
-	Joystick padOperator = new Joystick(2);
+	
+	DigitalInput linearHookLimitSwitch;
+		
+	//Controllers
+	Joystick driverController = new Joystick(1);
+	Joystick operatorController = new Joystick(2);
+	
+	//Encoders
 	Encoder frontRightEncoder = new Encoder( 2 , 3 );
 	Encoder frontLeftEncoder = new Encoder ( 0 , 1 );
 	Encoder strafeEncoder = new Encoder ( 6 , 7 );
 	Encoder linearSlideEncoder = new Encoder ( 4 , 5 );
+	
+	//magic numbers
+	int slowButton = 1;
+	double slowDriveSpeedFactor = 2.0;
+	double slowLinearSpeedFactor = 2.0;
+	int linearSlideAutoButton1 = 7;
+	int linearSlideAutoButton2 = 8;
+	int containerButton = 6;
+	int totePosition1 = 1;
+	int totePosition2 = 2;
+	int totePosition3 = 3;
+	int totePosition4 = 4;
+	double dPadSpeed = 0.5;
+	double encoderRotation = 360 ;
+	
+	//
 	int state = 0;
+	
+	//
 	int backDistance = 0;
 	int forwardDistance = 0;
 	int leftDistance = 0;
@@ -49,14 +71,32 @@ public class Robot extends IterativeRobot
 	int downDistance = 0;
 	int counter = 1;
 	int autonDistance = 0;
+	
+	//
 	int forwardSpeed = 1;
 	int backSpeed = -1;
-	int slideSpeedUp = 1;
-	int slideSpeedDown = -1;
 	int leftSpeed = 1;
 	int rightSpeed = 1;
 	int stop = 0;
-	int diameter = 0;
+	double slideSpeedUp = 0.5;
+	double slideSpeedDown = -0.5;
+	
+	//
+	double leftControlSpeed;
+	double rightControlSpeed;
+	double strafeControlSpeed;
+	
+	//Linear Slide variables
+	double toteHeight = 0;
+	double linearSlideGroundHeight = 0;
+	int button = 0;
+	double thumbStickTolerance = 0.1;
+	
+	
+	//
+	double diameter = 0.0;
+	double desiredPreset;
+	double desired;
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 		
@@ -71,11 +111,8 @@ public class Robot extends IterativeRobot
     	}
 		return desiredPosition;
     }
-
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
+    
+    //runs at the beginning of the program
     public void robotInit() 
     {
 		oi = new OI();
@@ -92,29 +129,25 @@ public class Robot extends IterativeRobot
 	{
 		Scheduler.getInstance().run();
 	}
-
+	
+	//Beginning of Autonomous Mode
     public void autonomousInit() 
     {
         // schedule the autonomous command (example)
         if (autonomousCommand != null) autonomousCommand.start();
+        
         frontRightEncoder.reset();
         frontLeftEncoder.reset();
         strafeEncoder.reset();
         linearSlideEncoder.reset();
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
+    
     public void autonomousPeriodic() 
     {
-    	if(linearSlideEncoder.get() <= 360 )
-    	{
-    		linearSlideMotor1.set( - linearSlideEncoder.get() );
-    		linearSlideMotor2.set( - linearSlideEncoder.get() );
-    	}
         Scheduler.getInstance().run();
-       // if(/*position left*/)
+        backDrive();
+        // if(/*position left*/)
         {
         	switch (state)
         	{
@@ -135,7 +168,7 @@ public class Robot extends IterativeRobot
         			forwardRightMotor.set(forwardSpeed);
         			backLeftMotor.set(forwardSpeed);
         			backRightMotor.set(forwardSpeed);
-        			if(frontLeftLimitSwitch.get() && frontRightLimitSwitch.get() )
+        			if(linearHookLimitSwitch.get())
         			{
         				counter ++;
         				state = 1;
@@ -253,7 +286,7 @@ public class Robot extends IterativeRobot
     			forwardRightMotor.set(forwardSpeed);
     			backLeftMotor.set(forwardSpeed);
     			backRightMotor.set(forwardSpeed);
-    			if(frontLeftLimitSwitch.get() && frontRightLimitSwitch.get() )
+    			if(linearHookLimitSwitch.get())
     			{
     				counter ++;
     				state = 1;
@@ -384,7 +417,7 @@ public class Robot extends IterativeRobot
         			forwardRightMotor.set(forwardSpeed);
         			backLeftMotor.set(forwardSpeed);
         			backRightMotor.set(forwardSpeed);
-        			if(frontLeftLimitSwitch.get() && frontRightLimitSwitch.get() )
+        			if( linearHookLimitSwitch.get() )
         			{
         				counter ++;
         				state = 1;
@@ -487,10 +520,6 @@ public class Robot extends IterativeRobot
     
     public void teleopInit() 
     {
-		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
         if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
@@ -503,6 +532,7 @@ public class Robot extends IterativeRobot
 
     }
     
+    //Makes sure the motor.set() value does not go past 1 or -1
     public double limit( double number )
     {
     	if(number > 1.0)
@@ -515,109 +545,276 @@ public class Robot extends IterativeRobot
     	}
 		return number;
     }
-    //if number is to large, set to one or negative one
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() 
+    
+    //might not be mine codes
+    public void encoderControl(int position)
+    {
+    	int height = 100; //placeholder value from bottom to first tote position
+    	if(linearSlideEncoder.get() < position * height){
+    		linearSlideMotor1.set(0.5);
+    		linearSlideMotor2.set(0.5);
+    	}
+    	if(linearSlideEncoder.get() > position * height){
+    		linearSlideMotor1.set(0.5);
+    		linearSlideMotor2.set(0.5);
+    	}
+    }
+    
+    //this is the driving code that will be used
+    public void controlDriving( double leftControlSpeed, double rightControlSpeed, double strafeControlSpeed )
+    {
+        backLeftMotor.set( leftControlSpeed ); 
+        forwardLeftMotor.set( leftControlSpeed ); 
+    	
+    	forwardRightMotor.set( -rightControlSpeed );
+        backRightMotor.set( -rightControlSpeed );
+        
+        strafeMotor1.set( strafeControlSpeed );
+        strafeMotor2.set( strafeControlSpeed );
+    }
+    
+    //this is the p-pad driver 
+    public void dPadDriver()
+    {
+    	int POV = driverController.getPOV();
+    	if( POV == 1 || POV == 2 || POV == 8 )
+    	{
+    		leftControlSpeed = dPadSpeed;
+    		rightControlSpeed = dPadSpeed;
+    	}
+    	else if( POV == 4 || POV == 5 || POV == 6 )
+    	{
+    		leftControlSpeed = -dPadSpeed;
+    		rightControlSpeed = -dPadSpeed;
+    	}
+    	else
+    	{
+    		leftControlSpeed = 0.0;
+    		rightControlSpeed = 0.0;
+    	}
+    	
+    	if( POV == 2 || POV == 3 || POV == 4 )
+    	{
+    		strafeControlSpeed = dPadSpeed;
+    	}
+    	else if( POV== 6 || POV == 7 || POV == 8 )
+    	{
+    		strafeControlSpeed = -dPadSpeed;
+    	}
+    	else
+    	{
+    		strafeControlSpeed = 0.0;
+    	}
+    }
+    
+    //this is the stick driver for the controller 
+    public void stickDrive()
+    {
+    	if(driverController.getRawButton(slowButton))
+    	{
+    		leftControlSpeed = limit( (driverController.getY() + driverController.getZ()) / slowDriveSpeedFactor );
+    		rightControlSpeed = limit( (driverController.getY() - driverController.getZ()) / slowDriveSpeedFactor );
+    		strafeControlSpeed = driverController.getX() / slowDriveSpeedFactor;
+    	}	
+    	else
+    	{
+    		leftControlSpeed = limit( driverController.getY() + driverController.getZ() );
+    		rightControlSpeed = limit( driverController.getY() - driverController.getZ() );
+    		strafeControlSpeed = driverController.getX();	
+    	}
+    }
+    
+    //this function adds all the drivers together into one super driver
+    public void masterDrive()
+    {
+        int POV = driverController.getPOV();
+        if( POV >= 1 )
+        {
+        	dPadDriver();
+        }
+        else 
+        {
+        	stickDrive();
+        }
+        controlDriving( leftControlSpeed, rightControlSpeed, strafeControlSpeed );
+    }
+    
+    //this is the point of view using hook drive 
+    public void dPadHookDrive()
+    {
+    	int POV = operatorController.getPOV();
+    	if( POV == 1)
+    	{
+    		linearSlideMotor1.set(slideSpeedUp);
+    		linearSlideMotor2.set(slideSpeedUp);    		
+    	}
+    	else
+    	{
+    		linearSlideMotor1.set(slideSpeedDown);
+    		linearSlideMotor2.set(slideSpeedDown);   
+    	}
+    }
+    
+    //driving with the stick
+    public void stickDriveHookDrive()
+    {
+    	double operator = operatorController.getY();
+    	linearSlideMotor1.set( operator );
+    	linearSlideMotor2.set( operator );
+    }
+    
+    //this controls all the drive code that the robot can do, such as d-pad and stick drive
+    public void masterManualLinearDrive()
+    {
+    	int POV = operatorController.getPOV();
+    	double operator = operatorController.getY();
+    	boolean button5 = operatorController.getRawButton(5);
+    	boolean button6 = operatorController.getRawButton(6);
+    	
+    	if( (button5 || button6) && linearHookLimitSwitch.get() )
+    	{
+    		linearSlideMotor1.set(slideSpeedUp);
+    		linearSlideMotor2.set(slideSpeedUp);
+    		desired = linearSlideEncoder.get();
+    	}
+    	else if( POV == 1 || POV == 5 )
+    	{
+    		dPadHookDrive();
+    		desired = linearSlideEncoder.get();
+    	}
+    	else if( operator <= -thumbStickTolerance || operator >= thumbStickTolerance )
+    	{
+    		stickDriveHookDrive();
+    		desired = linearSlideEncoder.get();
+    	}
+    	else
+    	{
+    		backDrive();
+    	}
+    }
+     
+    //this controls the master slide and goes through a switch go thorough the switch statement
+    public void masterLinearSlide()
+    {
+    	boolean button1 = operatorController.getRawButton(1);
+    	boolean button2 = operatorController.getRawButton(2);
+    	boolean button3 = operatorController.getRawButton(3);
+    	boolean button4 = operatorController.getRawButton(4);
+    	int POV = operatorController.getPOV();
+    	double operator = operatorController.getY();
+    	int newButton;
+    	int state = 0;
+    	
+    	/*
+    	 * if button 1 - 4 is pressed
+    	 * it'll change the newButton to the button
+    	 * it then goes through a cycle to see weather if its > than or < than the desire presets
+    	 */
+    	if( button1 || button2 || button3 || button4 )
+		{
+			if(button4)
+			{
+				newButton = 4;
+			}
+			else if(button3)
+			{
+				newButton = 3;
+			}
+			else if(button2)
+			{
+				newButton = 2;
+			}
+			else
+			{
+				newButton = 1;
+			}
+			
+			if( newButton != button )
+	    	{
+				button = newButton;
+				desiredPreset = button * toteHeight - linearSlideGroundHeight;
+				if( linearSlideEncoder.get() < desiredPreset) 
+				{
+					state = 2;
+				}
+				else if( linearSlideEncoder.get() > desiredPreset )
+				{
+					state = 1;
+				}
+				else
+				{
+					state = 0;
+				}
+				//update states
+	    	}
+		}
+    	
+    	if( POV == 1 || POV == 5 || operator <= -thumbStickTolerance || operator >= thumbStickTolerance )
+    	{
+    		state = 0;
+    	}
+    	
+    	if( state != 0 )
+    	{
+    		desired = linearSlideEncoder.get();
+    	}
+    	
+    	switch(state)
+    	{
+    	
+    	case 0:
+    		masterManualLinearDrive();
+    		break;
+    		
+    	case 1:
+    		
+    		linearSlideMotor1.set(slideSpeedDown);
+    		linearSlideMotor2.set(slideSpeedDown);
+    		
+    		if( linearSlideEncoder.get() <= desiredPreset )
+    		{
+    			desired = linearSlideEncoder.get();
+    			state = 0;
+    		}
+    		break;    		
+    		
+    	case 2:
+    		
+    		linearSlideMotor1.set(slideSpeedUp);
+    		linearSlideMotor2.set(slideSpeedUp);
+    		    		
+    		if( linearSlideEncoder.get() >= desiredPreset )
+    		{
+    			desired = linearSlideEncoder.get();
+    			state = 0;
+    		}
+    		break;
+    	}
+    }
+    
+    //this functions is for back driving 
+    public void backDrive()
+    {
+       	if( linearSlideEncoder.get() <= desired )
+    	{
+    		linearSlideMotor1.set( limit( linearSlideEncoder.get() / encoderRotation ) );
+    		linearSlideMotor2.set( limit( linearSlideEncoder.get() / encoderRotation ) );
+    	}
+    	else
+    	{
+    		linearSlideMotor1.set(0);
+    		linearSlideMotor2.set(0);
+    	}
+    }
+    
+    //Operator and driving starts and moving motors
+    public void teleopPeriodic()
     {
         Scheduler.getInstance().run();
         
-        if(padDriver.getRawButton(1))
-        {
-        	double frontLeftSpeed = limit( (padDriver.getY() + padDriver.getZ()) / 2.0 );
-        	double frontRightSpeed = limit( (padDriver.getY() - padDriver.getZ()) / 2.0 );
-        	double backLeftSpeed = limit( (padDriver.getY() + padDriver.getZ()) / 2.0 ) ;
-        	double backRightSpeed = limit( (padDriver.getY() - padDriver.getZ()) / 2.0 );
-        	double strafeSpeed = padDriver.getX() / 2.0;
-        	forwardLeftMotor.set( Math.sqrt(frontLeftSpeed) ); 
-        	forwardRightMotor.set( Math.sqrt(frontRightSpeed) );
-        	backLeftMotor.set( Math.sqrt(backLeftSpeed) ); 
-        	backRightMotor.set( Math.sqrt(backRightSpeed) );
-        	strafeMotor1.set( Math.sqrt(strafeSpeed) );
-        	strafeMotor2.set( Math.sqrt(strafeSpeed) );
-        	//moving at half the speed 
-        }	
-        else
-        {
-        	double frontLeftSpeed = limit( padDriver.getY() + padDriver.getZ() );
-        	double frontRightSpeed = limit( padDriver.getY() - padDriver.getZ() );
-        	double backLeftSpeed = limit(padDriver.getY() + padDriver.getZ() );
-        	double backRightSpeed = limit( padDriver.getY() - padDriver.getZ() );
-        	double strafeSpeed = padDriver.getX();
-        	forwardLeftMotor.set( Math.sqrt(frontLeftSpeed) ); 
-        	forwardRightMotor.set( Math.sqrt(frontRightSpeed) );
-        	backLeftMotor.set( Math.sqrt(backLeftSpeed) ); 
-        	backRightMotor.set( Math.sqrt(backRightSpeed) );     
-        	strafeMotor1.set( Math.sqrt(strafeSpeed) );
-        	strafeMotor2.set( Math.sqrt(strafeSpeed) );
-        	//moving at full speed 
-        }
-        //Do Back Drive Soon
-        if(padOperator.getRawButton(1))
-        {
-        	double linearSlideSpeed = limit(padOperator.getY() / 2.0);
-        	linearSlideMotor1.set( linearSlideSpeed );
-        	linearSlideMotor2.set( linearSlideSpeed );
-        	//linear slide moves determined by the position of the Y axis on the operator pad 
-        }
-        else
-        {
-        	double linearSlideSpeed = limit(padOperator.getY());
-        	linearSlideMotor1.set( linearSlideSpeed );
-        	linearSlideMotor2.set( linearSlideSpeed );
-        	//linear slide moves determined by the position of the Y axis on the operator pad 
-        }
-        
-        if(padOperator.getRawButton(7) || padOperator.getRawButton(8))
-        {
-        	if(frontLeftLimitSwitch.get() && frontLeftLimitSwitch.get())
-        	{
-        		linearSlideMotor1.set(slideSpeedUp);
-        		linearSlideMotor2.set(slideSpeedUp);
-        	}
-        }
-        else
-        {
-        	linearSlideMotor1.set(stop);
-        	linearSlideMotor2.set(stop);
-        }
-        
-        if(padOperator.getRawButton(1))
-        {
-        	//set tote position 1
-        }
-        else if(padOperator.getRawButton(1) && padOperator.getRawButton(6))
-        {
-        	//set tote plus container position 1
-        }
-        
-        if(padOperator.getRawButton(2))
-        {
-        	//set tote position 2
-        }
-        else if(padOperator.getRawButton(2) && padOperator.getRawButton(6))
-        {
-        	//set tote plus container position 2
-        }
-        
-        if(padOperator.getRawButton(3))
-        {
-        	//set tote position 3
-        }
-        else if(padOperator.getRawButton(3) && padOperator.getRawButton(6))
-        {
-        	//set tote plus container position 3
-        }
-        
-        if(padOperator.getRawButton(4))
-        {
-        	//set tote position 4
-        }
-        else if(padOperator.getRawButton(4) && padOperator.getRawButton(6))
-        {
-        	//set tote plus container position 4
-        }
+        masterDrive();
+        masterLinearSlide();
+        backDrive();
     }
     
     /**
